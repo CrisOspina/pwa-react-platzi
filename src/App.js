968d2424c09b31cc003fdc4b063315908dfb85d6
Cristian Ospina <crisospina523@gmail.com>
2019-09-03 18:05:43 -0500

    component main

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Router, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Recipe from './pages/Recipe';
+import Timer from './pages/Timer';
+import IfOffline from './components/IfOffline';
+import './App.css';
+
+// Google Analytics
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+
+const history = createBrowserHistory();
+
+ReactGA.initialize('UA-00000-01');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+history.listen(function(location) {
+  ReactGA.pageview(window.location.pathname + window.location.search);
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router history={history}>
+        <div>
+          <header>
+            <Link to="/">
+              Recetas <IfOffline>Offline</IfOffline>
+            </Link>
+            <Link to="/timer" className="timerLink">
+              ⏱
+            </Link>
+          </header>
+
+          <main>
+            <Route exact path="/" component={Home} />
+            <Route path="/recipe/:recipeId" component={Recipe} />
+            <Route path="/timer" component={Timer} />
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
